083a1b6ff462875dac284164e05861abd56d43f4
Saint48198 <daniels.scott.r@gmail.com>
2024-12-11 07:00:26 -0500

    fixed missing pins on the map

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -2,14 +2,26 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
+import L from 'leaflet';
 import styles from './Map.module.scss';
+
+// Fix the default icon path
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
+});
 
 const Map = () => {
   // Set the initial center of the map and zoom level
-  const center: LatLngExpression = [51.505, -0.09];
+  const center: LatLngExpression = [39.8283, -98.5795]; // Center of North America (approximate)
+  const zoom = 3; // Zoomed out to show all of North America
 
   return (
-    <MapContainer center={center} zoom={13} className={styles.mapContainer}>
+    <MapContainer center={center} zoom={zoom} className={styles.mapContainer}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
