c926c932c71499a4d15566d5910b6683a3fd00f8
Saint48198 <daniels.scott.r@gmail.com>
2025-01-28 11:23:41 -0500

    converted to normal sass from tailwind

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Message.module.scss';
 
 interface MessageProps {
   message: string;
@@ -10,14 +11,16 @@ const Message: React.FC<MessageProps> = ({ message, type }) => {
 
   const messageClass =
     type === 'success'
-      ? 'text-green-700 bg-green-100 border-green-400'
-      : 'text-red-700 bg-red-100 border-red-400';
+      ? styles.messageSuccess
+      : type === 'error'
+        ? styles.messageError
+        : '';
 
   return (
     <div
       role="alert"
       aria-live="assertive"
-      className={`mb-4 p-4 border rounded ${messageClass}`}
+      className={`${styles.message} ${messageClass}`}
     >
       {message}
     </div>
