57aedfd96bd15410cf38fdf1bc67fe6f0360dfd1
Saint48198 <daniels.scott.r@gmail.com>
2025-01-28 09:12:34 -0500

    converted the loader spinner from tailwind classes to plain sass and created util class for sr-only

--- components/LoadingSpinner/LoadingSpinner.tsx
+++ components/LoadingSpinner/LoadingSpinner.tsx
@@ -4,20 +4,16 @@ import styles from './LoadingSpinner.module.scss';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <div
-      role={'status'}
-      aria-live={'polite'}
-      className="flex justify-center items-center space-x-2 mt-2"
-    >
+    <div role={'status'} aria-live={'polite'} className={styles.loadingSpinner}>
       <svg
-        className="w-14 h-14 animate-spin text-blue-600"
+        className={styles.spinnerIcon}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         aria-hidden="true"
       >
         <circle
-          className="opacity-25"
+          className={styles.spinnerCircle}
           cx="12"
           cy="12"
           r="10"
@@ -25,7 +21,7 @@ const LoadingSpinner: React.FC = () => {
           strokeWidth="4"
         ></circle>
         <path
-          className="opacity-75"
+          className={styles.spinnerPath}
           fill="currentColor"
           d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
         ></path>
