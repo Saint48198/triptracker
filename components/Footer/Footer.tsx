fdab34d6a1bf245a9051281eee3ad364432a6294
Saint48198 <daniels.scott.r@gmail.com>
2025-01-28 16:42:13 -0500

    switched to using sass over tailwind

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
+import styles from './Footer.module.scss';
+
 const Footer = () => {
   return (
-    <footer className="bg-white-800 text-black-300 py-4">
-      <div className="container mx-auto flex flex-col items-end justify-center space-y-2">
-        <p className="text-sm">
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p className={styles.text}>
           &copy; {new Date().getFullYear()} Scott Daniels. All rights reserved.
         </p>
       </div>
