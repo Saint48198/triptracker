1346b0b0307d1c23adb8293b9277921d7581c8f5
Saint48198 <daniels.scott.r@gmail.com>
2025-01-29 13:37:59 -0500

    converted to SASS

--- components/LatLngField/LatLngField.tsx
+++ components/LatLngField/LatLngField.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaSpinner } from 'react-icons/fa';
+import styles from './LatLngField.module.scss';
+import Button from '@/components/Button/Button';
 
 interface LatLngFieldProps {
   latLabel: string;
@@ -23,13 +25,10 @@ const LatLngField: React.FC<LatLngFieldProps> = ({
   onLookup,
 }) => {
   return (
-    <div className="mx-auto">
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center space-x-2">
-          <label
-            htmlFor="lat"
-            className="text-gray-700 font-medium whitespace-nowrap"
-          >
+    <div className={styles.latLngField}>
+      <div className={styles.fieldGroup}>
+        <div className={styles.field}>
+          <label htmlFor="lat" className={styles.label}>
             {latLabel}:
           </label>
           <input
@@ -37,17 +36,14 @@ const LatLngField: React.FC<LatLngFieldProps> = ({
             id="lat"
             value={lat || ''}
             onChange={(e) => onLatChange(Number(e.target.value))}
-            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+            className={styles.input}
             readOnly={isLoading}
             required
           />
         </div>
 
         <div className="flex items-center space-x-2">
-          <label
-            htmlFor="lng"
-            className="text-gray-700 font-medium whitespace-nowrap"
-          >
+          <label htmlFor="lng" className={styles.label}>
             {lngLabel}:
           </label>
           <input
@@ -55,23 +51,21 @@ const LatLngField: React.FC<LatLngFieldProps> = ({
             id="lng"
             value={lng || ''}
             onChange={(e) => onLngChange(Number(e.target.value))}
-            className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+            className={styles.input}
             readOnly={isLoading}
             required
           />
         </div>
-
-        <button
-          type="button"
+        <Button
+          ariaLabel="Look up Lat/Lng"
           onClick={onLookup}
-          className={`${
-            isLoading ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500'
-          } text-white px-4 py-2 rounded flex items-center`}
-          disabled={isLoading}
+          isDisabled={isLoading}
+          styleType="primary"
+          buttonType={'button'}
         >
           {isLoading ? <FaSpinner className="animate-spin mr-2" /> : null}
           {isLoading ? 'Loading...' : 'Look up Lat/Lng'}
-        </button>
+        </Button>
       </div>
     </div>
   );
