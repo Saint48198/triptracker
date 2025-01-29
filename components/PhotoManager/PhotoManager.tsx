bbe673ce89b063eb134cd9de341b7f0627d7cbeb
Saint48198 <daniels.scott.r@gmail.com>
2025-01-29 14:31:33 -0500

    convert to SASS  and code clean up

--- components/PhotoManager/PhotoManager.tsx
+++ components/PhotoManager/PhotoManager.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   bulkAddPhotos,
   bulkRemovePhotos,
@@ -7,6 +7,9 @@ import {
 import { Photo, PhotoManagerProps } from '@/types/PhotoTypes';
 import ImageButton from '@/components/ImageButton/ImageButton';
 import { getTransformedImageUrl } from '@/utils/imageUtils';
+import styles from './PhotoManager.module.scss';
+import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
+import Button from '@/components/Button/Button';
 
 const PhotoManager: React.FC<PhotoManagerProps> = ({
   entityType,
@@ -18,63 +21,60 @@ const PhotoManager: React.FC<PhotoManagerProps> = ({
   const [selectedPhotos, setSelectedPhotos] = useState<Photo[]>([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setPhotos(initialPhotos);
-  }, [initialPhotos]);
+  const handleAddRemovePhotos = useCallback(
+    async (newPhotos: Photo[]) => {
+      setLoading(true);
 
-  useEffect(() => {
-    if (Array.isArray(externalPhotos)) handleAddRemovePhotos(externalPhotos);
-  }, [externalPhotos]);
+      const addPhotos = newPhotos.filter(
+        (newPhoto: Photo) =>
+          !photos.some((photo) => photo.photo_id === newPhoto.photo_id)
+      );
+      const removePhotos = photos.filter(
+        (photo: Photo) =>
+          !newPhotos.some((newPhoto) => newPhoto.photo_id === photo.photo_id)
+      );
 
-  const handleAddRemovePhotos = async (newPhotos: Photo[]) => {
-    setLoading(true);
+      if (addPhotos.length > 0) {
+        const addPayload: PhotoPayload = {
+          entityType,
+          entityId,
+          photos: addPhotos,
+        };
 
-    const addPhotos = newPhotos.filter(
-      (newPhoto: Photo) =>
-        !photos.some((photo) => photo.photo_id === newPhoto.photo_id)
-    );
-    const removePhotos = photos.filter(
-      (photo: Photo) =>
-        !newPhotos.some((newPhoto) => newPhoto.photo_id === photo.photo_id)
-    );
-
-    if (addPhotos.length > 0) {
-      const addPayload: PhotoPayload = {
-        entityType,
-        entityId,
-        photos: addPhotos,
-      };
-
-      try {
-        await bulkAddPhotos(addPayload);
-        setPhotos([...photos, ...addPhotos]);
-      } catch (error) {
-        console.error('Failed to add photos:', error);
+        try {
+          await bulkAddPhotos(addPayload);
+          setPhotos([...photos, ...addPhotos]);
+        } catch (error) {
+          console.error('Failed to add photos:', error);
+        }
       }
-    }
 
-    if (removePhotos.length > 0) {
-      const removePayload: PhotoPayload = {
-        entityType,
-        entityId,
-        photos: removePhotos,
-      };
+      if (removePhotos.length > 0) {
+        const removePayload: PhotoPayload = {
+          entityType,
+          entityId,
+          photos: removePhotos,
+        };
 
-      try {
-        await bulkRemovePhotos(removePayload);
-        setPhotos(
-          photos.filter(
-            (photo) =>
-              !removePhotos.some((removePhoto) => removePhoto.url === photo.url)
-          )
-        );
-      } catch (error) {
-        console.error('Failed to remove photos:', error);
+        try {
+          await bulkRemovePhotos(removePayload);
+          setPhotos(
+            photos.filter(
+              (photo) =>
+                !removePhotos.some(
+                  (removePhoto) => removePhoto.url === photo.url
+                )
+            )
+          );
+        } catch (error) {
+          console.error('Failed to remove photos:', error);
+        }
       }
-    }
 
-    setLoading(false);
-  };
+      setLoading(false);
+    },
+    [entityId, entityType, photos]
+  );
 
   const togglePhotoSelection = (photo: Photo) => {
     if (loading) return;
@@ -87,15 +87,23 @@ const PhotoManager: React.FC<PhotoManagerProps> = ({
     }
   };
 
+  useEffect(() => {
+    setPhotos(initialPhotos);
+  }, [initialPhotos]);
+
+  useEffect(() => {
+    if (Array.isArray(externalPhotos)) handleAddRemovePhotos(externalPhotos);
+  }, [externalPhotos, handleAddRemovePhotos]);
+
   return (
     <div>
       {photos.length > 0 ? (
         <div>
           <div>
             {loading ? (
-              <div className="loading-icon">Processing...</div>
+              <LoadingSpinner />
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+              <div className={styles.grid}>
                 {photos.map((photo: Photo, index) => (
                   <div key={index}>
                     <ImageButton
@@ -111,15 +119,16 @@ const PhotoManager: React.FC<PhotoManagerProps> = ({
               </div>
             )}
           </div>
-          <button
-            type={'button'}
+          <Button
+            ariaLabel={'Add/Remove Selected Photos'}
+            buttonType={'button'}
             onClick={() => handleAddRemovePhotos(selectedPhotos)}
           >
-            Add/Remove Selected Photos
-          </button>
+            Add/Remove Selected
+          </Button>
         </div>
       ) : (
-        <p className="mt-6 text-gray-500">No photos attached to this city.</p>
+        <p className={styles.noPhotos}>No photos attached to this city.</p>
       )}
     </div>
   );
