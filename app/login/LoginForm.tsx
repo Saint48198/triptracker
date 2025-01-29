0a79b2ec44e5a5337494139d0ff87ca3d8bae003
Saint48198 <daniels.scott.r@gmail.com>
2025-01-29 09:12:58 -0500

    converted to SASS and updated to use message and button components

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -3,6 +3,9 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import Message from '@/components/Message/Message';
+import styles from './LoginForm.module.scss';
+import Button from '@/components/Button/Button';
 
 const LoginForm: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -28,21 +31,20 @@ const LoginForm: React.FC = () => {
       if (response.status === 200) {
         router.push('/'); // Redirect to the homepage or dashboard
       }
-    } catch (err: any) {
+    } catch (err: unknown) {
       setError(
-        err.response?.data?.error || 'Failed to login. Please try again.'
+        err instanceof Error
+          ? err.message
+          : 'Failed to login. Please try again.'
       );
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-      <div className="mb-4">
-        <label
-          htmlFor="username"
-          className="block text-sm font-medium text-gray-700"
-        >
+      {error && <Message message={error} type={'error'}></Message>}
+      <div className={styles.formGroup}>
+        <label htmlFor="username" className={styles.label}>
           Username
         </label>
         <input
@@ -50,14 +52,11 @@ const LoginForm: React.FC = () => {
           id="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          className={styles.input}
         />
       </div>
-      <div className="mb-6">
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
+      <div className={styles.formGroup}>
+        <label htmlFor="password" className={styles.label}>
           Password
         </label>
         <input
@@ -65,15 +64,12 @@ const LoginForm: React.FC = () => {
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          className={styles.input}
         />
       </div>
-      <button
-        type="submit"
-        className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
+      <Button ariaLabel={'Login'} buttonType={'submit'} styleType={'primary'}>
         Login
-      </button>
+      </Button>
     </form>
   );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
 import LoginForm from './LoginForm';
+import styles from './LoginPage.module.scss';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6">Login</h2>
+    <div className={styles.loginPage}>
+      <div className={styles.loginContainer}>
+        <h2 className={styles.loginTitle}>Login</h2>
         <LoginForm />
       </div>
     </div>
