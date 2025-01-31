dd1b4a0d52cc85cae481dc8a5b66a127b9f9434f
Saint48198 <daniels.scott.r@gmail.com>
2025-01-31 08:49:37 -0500

    removed unused components

--- components/ClientButtonWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import Button from '@/components/Button';
-
-export default function ClientButtonWrapper() {
-  return <Button onClick={() => alert('Button clicked!')}>Get Started</Button>;
-}
