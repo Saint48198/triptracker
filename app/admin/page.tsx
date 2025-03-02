d195be3236abcc7fc7716de8f0772e4d1d4ec3ce
Saint48198 <daniels.scott.r@gmail.com>
2025-03-02 18:19:50 -0500

    add an icon lib and updated the colors and include icons for the admin site grid on the admin landing page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 import styles from './AdminLandingPage.module.scss';
+import { Building, Earth, ImageUp, Landmark, Map, User } from 'lucide-react';
 
 export default function AdminLandingPage() {
   return (
@@ -16,36 +17,42 @@ export default function AdminLandingPage() {
           <div className={styles.adminContentContainer}>
             <Link href="/admin/countries" className={styles.link}>
               Manage Countries
+              <Earth className={styles.iconBackground} />
             </Link>
             <Link
               href="/admin/states"
               className={`${styles.link} ${styles.linkGreen}`}
             >
               Manage States/Provinces
+              <Map className={styles.iconBackground} />
             </Link>
             <Link
               href="/admin/cities"
-              className={`${styles.link} ${styles.linkGreen}`}
+              className={`${styles.link} ${styles.linkYellow}`}
             >
               Manage Cities
+              <Building className={styles.iconBackground} />
             </Link>
             <Link
               href="/admin/attractions"
-              className={`${styles.link} ${styles.linkPurple}`}
+              className={`${styles.link} ${styles.linkOrange}`}
             >
               Manage Attractions
+              <Landmark className={styles.iconBackground} />
             </Link>
             <Link
               href="/admin/users"
-              className={`${styles.link} ${styles.linkYellow}`}
+              className={`${styles.link} ${styles.linkPurple}`}
             >
               Manage Users
+              <User className={styles.iconBackground} />
             </Link>
             <Link
               href={'/admin/image/upload'}
-              className={`${styles.link} ${styles.linkPurple}`}
+              className={`${styles.link} ${styles.linkGray}`}
             >
               Upload Images
+              <ImageUp className={styles.iconBackground} />
             </Link>
           </div>
         </div>
